4975559db228cdb79b8de2d8205b4284fe69cb59
wlstmf <wlstmf@LAPTOP-3KIS14LU>
2020-04-06 00:08:58 +0900

    0406

--- src/Q01.java
+++ src/Q01.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Q01 {
 /*정렬알고리즘 - 버블정렬(Bubble Sort) 
 1. 중복되지 않는 난수 10개(1~99)를 생성하여 크기가 10인 배열에 담는다.
@@ -6,9 +9,61 @@ public class Q01 {
 4.단, 정렬되는 과정을 모두 출력해야 한다.
 5.위 그림은 버블정렬을 이용하여 오름차순으로 정렬하는 과정이다.
  */
+	public static void bubble(int[] arr) {
+		
+		int index = 0;
+
+		for (int j = 0; j < arr.length; j++) {
+			System.out.print("Sort"+ (j+1) + "번 진행 : ");
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] < arr[i + 1]) {
+				} else if (arr[i] > arr[i + 1]) {
+					index = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = index;
+				}
+			}
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]+ " ");
+			}
+			System.out.println();
+		}
+
+	}
+	
 	public static void main(String[] args) {
 
+		Random ran = new Random();
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[10];
+		System.out.print("배열보기 : ");
+
+		boolean flag = true;
+
+		for (int i = 0; i < arr.length;) {
+			int num = ran.nextInt(11) + 1;
+
+			for (int j = 0; j < arr.length; j++) {
+				if (num == arr[j]) {
+					flag = false;
+					break;
+				}
+				flag = true;
+			}
+			if (flag) {
+				arr[i] = num;
+				i++;
+			}
+
+		}
 		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+		bubble(arr);
 		
 	}
 }
--- src/Qu02.java
+++ src/Qu02.java
@@ -1,57 +1,112 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class Qu02 {
-	/*정렬알고리즘 - 선택정렬(selection sort)
+	/* 정렬알고리즘 - 선택정렬(selection sort)
 	 1. 1~100 사이의 중복되지 않는 정수 10개의 난수를 생성한다.
 	 2.생성한 난수는 크기 10인 배열에 저장한다.
-	 3.위 그림을 참조하여 선택정렬로 오름차순 정렬한다. 
-	 4.정렬한 결과를 출력한다. 
-	 5.단 정렬되는 과정을 모두 출력해야 한다.
-	 */
-	public static void main(String[] args) {
+	 3.위 그림을 참조하여 선택정렬로 오름차순 정렬한다.
+	 4.정렬한 결과를 출력한다. 5.단 정렬되는 과정을 모두 출력해야 한다.*/
+	public static void min(int[] arr) {
 
-		int[] arr = new int[10];
+		int count = 0;
+		int index = 0;
+
+		for (int j = 0; j < 9; j++) {
+			// 최소값 설정
+			int min = 100;
+			for (int i = count; i < arr.length; i++) {
+				if (arr[i] < min) {
+					min = arr[i];
+					index = i;
+				}
+			}
+			arr[index] = arr[count];
+			arr[count] = min;
+			count++;
+
+			System.out.printf("Sort %d번 진행 :", j + 1);
+
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void max(int[] arr) {
+
+		int count = 0;
+		int index = 0;
+
+		for (int j = 0; j < 9; j++) {
+			// 최대값 설정
+			int max = 0;
+			for (int i = count; i < arr.length; i++) {
+				if (arr[i] > max) {
+					max = arr[i];
+					index = i;
+				}
+			}
+			arr[index] = arr[count];
+			arr[count] = max;
+			count++;
+
+			System.out.printf("Sort %d번 진행 :", j + 1);
+
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
 
 		Random ran = new Random();
 		Scanner sc = new Scanner(System.in);
 
+		int[] arr = new int[10];
 		System.out.print("배열보기 : ");
-		
-		for(int i=0; i<arr.length;i++) {
-			int num = ran.nextInt(100)+1;
-			arr[i]=num;
-			System.out.print(arr[i]+ " ");
+
+		boolean flag = true;
+
+		for (int i = 0; i < arr.length;) {
+			int num = ran.nextInt(11) + 1;
+
+			for (int j = 0; j < arr.length; j++) {
+				if (num == arr[j]) {
+					flag = false;
+					break;
+				}
+				flag = true;
+			}
+			if (flag) {
+				arr[i] = num;
+				i++;
+			}
+
 		}
+		for (int j = 0; j < arr.length; j++) {
+
+			System.out.print(arr[j] + " ");
+		}
+
 		System.out.println();
-		
+
 		System.out.println("정렬방식을 선택하세요");
 		System.out.println("1.오름차순");
 		System.out.println("2.내림차순");
 		System.out.print("선택 :");
 		int choice = sc.nextInt();
-		
+
 		switch (choice) {
 		case 1:
-			System.out.println("Sort1번 진행 :");
-			System.out.println("Sort2번 진행 :");
-			System.out.println("Sort3번 진행 :");
-			System.out.println("Sort4번 진행 :");
-			System.out.println("Sort5번 진행 :");
-			System.out.println("Sort6번 진행 :");
-			System.out.println("Sort7번 진행 :");
-			System.out.println("Sort8번 진행 :");
-			System.out.println("Sort9번 진행 :");
-			
+			min(arr);
 			break;
 		case 2:
+			max(arr);
 			break;
 
 		}
-		
-		
-		
-		
-
 	}
 }
